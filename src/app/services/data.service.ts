cb5d572a1c079bfcf7875ab534c489d4a9154004
Sreekar <kurudisreekar@gmail.com>
2024-09-04 12:47:44 +0530

    updated data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -30,28 +30,6 @@ export class DataService {
   userOneSubmissions$ = this.userOneSubmissionsSubject.asObservable();
   userTwoSubmissions$ = this.userTwoSubmissionsSubject.asObservable();
 
-  oneSubmitted(userOne: User, userOneSubmissions: Submission[]) {
-    this.userOne = userOne;
-    this.userOneSubmissions = userOneSubmissions;
-    if (this.userOne && this.userTwo) {
-      this.userOneSubject.next(this.userOne);
-      this.userTwoSubject.next(this.userTwo);
-      this.userOneSubmissionsSubject.next(this.userOneSubmissions);
-      this.userTwoSubmissionsSubject.next(this.userTwoSubmissions);
-    }
-  }
-
-  twoSubmitted(userTwo: User, userTwoSubmissions: Submission[]) {
-    this.userTwo = userTwo;
-    this.userTwoSubmissions = userTwoSubmissions;
-    if (this.userTwo && this.userOne) {
-      this.userOneSubject.next(this.userOne);
-      this.userTwoSubject.next(this.userTwo);
-      this.userOneSubmissionsSubject.next(this.userOneSubmissions);
-      this.userTwoSubmissionsSubject.next(this.userTwoSubmissions);
-    }
-  }
-
   getData(one: User, two: User) {
     console.log('Inside Get Data')
     this.userOne = one;
